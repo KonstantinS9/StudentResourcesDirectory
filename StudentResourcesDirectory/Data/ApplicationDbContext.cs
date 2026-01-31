34ad32b37c723f6e14788d1b70e8edbf2b948872
Konstantin <User@DESKTOP-E9IRBG2>
2026-01-31 23:34:58 +0200

    Configured DbContext

--- StudentResourcesDirectory/Data/ApplicationDbContext.cs
+++ StudentResourcesDirectory/Data/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using StudentResourcesDirectory.Models;
 
 namespace StudentResourcesDirectory.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+        public virtual DbSet<Resource> Resources { get; set;  } = null!;
+        public virtual DbSet<Category> Categories { get; set;  } = null!;
+        public virtual DbSet<Student> Students { get; set;  } = null!;
     }
 }
