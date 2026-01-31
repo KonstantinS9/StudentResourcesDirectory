834b5ad76afe2a91be2a0ea430bc4ba285c03157
Konstantin <User@DESKTOP-E9IRBG2>
2026-01-31 23:22:28 +0200

    Added "Student" entity

---
+++ StudentResourcesDirectory/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentResourcesDirectory.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        public string FacultyNumber { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        public DateTime RegisteredOn { get; set; }
+    }
+}
