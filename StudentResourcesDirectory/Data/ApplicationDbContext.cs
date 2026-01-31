bf8ba1ab0c8b15945578addb947bcb646043d02e
Konstantin <User@DESKTOP-E9IRBG2>
2026-01-31 23:49:25 +0200

    Edited DbContext

--- StudentResourcesDirectory/Data/ApplicationDbContext.cs
+++ StudentResourcesDirectory/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using StudentResourcesDirectory.Data.Configuration;
 using StudentResourcesDirectory.Models;
 
 namespace StudentResourcesDirectory.Data
@@ -9,5 +10,14 @@ namespace StudentResourcesDirectory.Data
         public virtual DbSet<Resource> Resources { get; set;  } = null!;
         public virtual DbSet<Category> Categories { get; set;  } = null!;
         public virtual DbSet<Student> Students { get; set;  } = null!;
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new ResourceConfiguration());
+            builder.ApplyConfiguration(new CategoryConfiguration());
+            builder.ApplyConfiguration(new StudentConfiguration);
+        }
     }
 }
